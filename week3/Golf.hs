9c02ee21645e998167c442f0e2eb32c1cfb293f5
Amit Rai <amit.rai@go-jek.com>
2020-02-13 09:30:11 +0530

    [AMit] Fix the comments for skip function

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -4,17 +4,18 @@ import Data.List
 
 {-
 skip function is an internal function which takes a list of type a and an index
-to skip (let's call this skip index) and returns an array with all the elements 
-skipped whose index are multiple of the skip index. In order to get index of 
-the elems in the list I have used zip function. 
+whose multiples should be included in the return list. That is any other index 
+that isn't the multiple of given index is excluded. 
+
+In order to get index of the elems in the list I have used zip function. 
 zip [1..] [9,10,11] = [(1,9), (2,10), (3,11)]. This returns a list
 of pair with first being the index and second being the element. I have started 
 from 1 based index because 0 `mod` anyNumber = 0 and the 1st elem will always be
 included.
 
 This list is passed to a filter which removes all the element whose index is
-divisble by the skip index. Since finally we only need list of a we take the
-second of the pair and return.
+not divisble by the givrn index. Since finally we only need list of Integers 
+we map over the list of pair and only take the second elem and finally return.
 -}
 
 skip :: [a] -> Int -> [a]
